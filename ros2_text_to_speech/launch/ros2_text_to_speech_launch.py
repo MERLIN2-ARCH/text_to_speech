73e5cfe5d75f59302417048567c4604fdf6f6390
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-01-06 11:32:41 +0100

    launch file created

---
+++ ros2_text_to_speech/launch/ros2_text_to_speech_launch.py
@@ -0,0 +1,31 @@
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import SetEnvironmentVariable
+
+
+def generate_launch_description():
+    pkg_name = 'ros2_text_to_speech'
+    namespace = pkg_name
+
+    stdout_linebuf_envvar = SetEnvironmentVariable(
+        'RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
+
+    #
+    # NODES
+    #
+
+    tts_node_cmd = Node(
+        package=pkg_name,
+        executable='tts_node',
+        name='tts_node',
+        namespace=namespace
+    )
+
+    ld = LaunchDescription()
+
+    ld.add_action(stdout_linebuf_envvar)
+
+    ld.add_action(tts_node_cmd)
+
+    return ld
